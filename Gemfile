76ab72762518f302001ccaac636b5195a0bc6f18
chriswang <wangguanzzz@126.com>
2018-08-12 21:59:03 +0800

    update

--- Gemfile
+++ Gemfile
@@ -5,3 +5,6 @@ source "https://gems.ruby-china.com"
 
 gem "aliyun-sdk","0.3.0"
 gem "rest-client"
+gem "mongo"
+gem "sinatra"
+gem "thin"
--- get_recent.rb
+++ get_recent.rb
@@ -1,18 +1,28 @@
 #get content
 require 'rest-client'
 require 'json'
+require 'mongo'
 TOKEN = '2.00cesKbC49eWNE3b55fa34f3ZIHDID'
 
-url = "https://api.weibo.com/2/statuses/home_timeline.json?access_token=#{TOKEN}&count=100&since_id=4271483859831950"
 
-res = RestClient.get url
-res = JSON.parse res
-weibos =  res['statuses']
-weibos.each do |weibo|
-	puts 'id'
-	puts weibo['id']
-#	puts "text:"
-#	puts weibo['text']
-#	puts "user:"
-#	puts weibo['user']['name']
+def insert_weibo(token,client,last_id = nil)
+  if last_id
+    url = "https://api.weibo.com/2/statuses/home_timeline.json?access_token=#{token}&count=100&since_id=#{last_id}"
+  else
+    url = "https://api.weibo.com/2/statuses/home_timeline.json?access_token=#{token}&count=100"
+  end
+  res = JSON.parse(RestClient.get(url)) 
+  weibos =  res['statuses']
+  
+  if weibos.empty?
+    return last_id
+  else
+    client[:weibo].insert_many weibos
+    return weibos.first['id']
+  end
 end
+
+client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'starmap')
+
+last_id = insert_weibo(TOKEN,client,'4272307796550988')
+puts last_id
