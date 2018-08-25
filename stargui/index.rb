c3a946e9bfe9c5425e613f17f69d7f3378e337bf
chriswang <wangguanzzz@126.com>
2018-08-25 22:27:55 +0800

    add basic auth

--- stargui/index.rb
+++ stargui/index.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
+#set :port, 80
+set :environment, :production
+
+use Rack::Auth::Basic, "starmap" do |username, password|
+  username == 'admin' and password == 'admin'
+end
 
 get '/' do 
   'hello world'
