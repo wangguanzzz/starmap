adc55565e7763ed18bde992e3e6910d937a0a344
Chris Wang <wangguanzzz@126.com>
2018-09-13 15:21:59 +0800

    upate

--- futures/test.js
+++ futures/test.js
@@ -1,3 +1,15 @@
-const https = require('https');
+
 const redis = require('redis');
-console.log('hello world');
+const request = require('request-promise-lite');
+
+const URL = 'http://hq.sinajs.cn/list=CU0';
+/*
+request.get('https://httpbin.org/get', { json: true })
+  .then((response) => {
+    console.log(JSON.stringify(response));
+  });
+*/
+request.get(URL,{ json: false })
+  .then((response) => {
+    console.log(response.toString());
+  });
